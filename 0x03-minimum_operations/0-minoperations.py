39befa04c9fa349a866321ee34f9cc573e1de3f1
fesus rocuts <fesusrocuts@gmail.com>
2020-08-11 22:44:02 -0500

    fixed input not integer, raise exception

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -15,27 +15,21 @@ def minOperations(n):
     fn minOperations(n), Returns an integer,
     If n is impossible to achieve, return 0
     """
-    operations = 0
-    if n < 2:
+    try:
+        operations = 0
+        if n < 2:
+            return operations
+
+        for i in range(2, int(n ** 0.5 + 1)):
+            # if MOD for n is 0
+            while n % i == 0:
+                operations += i
+                # floor div
+                n //= i
+
+        if n >= 2:
+            operations += n
+        # result
         return operations
-
-    for i in range(2, int(n ** 0.5 + 1)):
-        # if MOD for n is 0
-        while n % i == 0:
-            operations += i
-            # floor div
-            n //= i
-
-    if n >= 2:
-        operations += n
-    # result
-    return operations
-
-"""
-n = 9
-
-H => Copy All (1 op) => Paste (2 op) => HH => 
-Paste (3 op) =>HHH => 
-Copy All (4 op) => Paste (5 op ) => HHHHHH => 
-Paste (6 op) => HHHHHHHHH
-"""
+    except Exception as e:
+        return 0
