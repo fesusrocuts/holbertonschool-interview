43fb1180a361522ddb197d0018d395e32848be4f
Fesus Rocuts <fesusrocuts@gmail.com>
2020-10-07 22:34:44 -0500

    2D Menger Sponge v1

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,41 @@
+#include "menger.h"
+
+/**
+ * menger - function that draws a 2D Menger Sponge
+ * @level: var equal to 3^level
+ */
+void menger(int level)
+{
+	int n, row, col;
+
+	n = pow(3, level);
+	for (row = 0; row < n; row++)
+	{
+		for (col = 0; col < n; col++)
+		{
+			fill(row, col);
+		}
+		printf("%c",'\n');
+	}
+}
+
+/**
+ * fill - function that fill when the vars are 1 using MOD
+ * @row: int receive
+ * @col: int receive
+ */
+void fill(int row, int col)
+{
+	char fill = '#';
+	// printf("row is %d",row);
+	// printf("col is %d",col);
+	// printf("%c",'\n');
+	while (row || col)
+	{
+		if (row % 3 == 1 && col % 3 == 1)
+			fill = ' ';
+		row = row / 3;
+		col = col / 3;
+	}
+	fwrite(&fill, 1, 2, stdout);
+}
---
+++ 0x0B-menger/menger.h
@@ -0,0 +1,10 @@
+#ifndef MENGER_H
+#define MENGER_H
+
+#include <stdio.h>
+#include <math.h>
+
+void fill(int row, int col);
+void menger(int level);
+
+#endif /* MENGER_H */
