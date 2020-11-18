edb8db56baa767831d5ffb82c90bc740b0272feb
Fesus Rocuts <fesusrocuts@gmail.com>
2020-11-17 19:12:36 -0500

    update task t0

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -1,53 +1,47 @@
-#include <stdio.h>
-#include "search.h"
+#include "search_algos.h"
 
+/**
+ * linear_skip - searches for a value in a sorted skip list of integers
+ * @list: pointer to the head of the skip list to search in.
+ * A node of the express lane is placed every index which is a multiple 
+ * of the square root of the size of the list
+ * @value: value to search for
+ * Return: pointer to the first node where value is located, or NULL on failure
+ */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	skiplist_t *skipprev, *skipnext, *mainptr;
+	skiplist_t *express;
 
-	if (list == NULL)
-		return (NULL);
-
-
-	skipprev = list;
-	skipnext = list->express;
-	while (skipnext != NULL)
+	if (list != NULL)
 	{
-		//defautl print
-		printf("Value checked at index [%lu] = [%d]\n", skipnext->index, skipnext->n);
-		if (skipnext->n >= value)
-			break;
-		skipprev = skipnext;
-		skipnext = skipprev->express;
+		express = list->express;
+		printf("Value checked at index [%lu] = [%d]\n", express->index,
+		       express->n);
+		while (express != NULL && express->n < value)
+		{
+			list = express;
+			express = express->express;
+			if (express == NULL)
+				break;
+			printf("Value checked at index [%lu] = [%d]\n", express->index,
+			       express->n);
+		}
+		if (express == NULL)
+		{
+			express = list;
+			while (express->next != NULL)
+				express = express->next;
+		}
+		printf("Value found between indexes [%lu] and [%lu]\n", list->index,
+		       express->index);
+		while (list != NULL && list->n <= express->n)
+		{
+			printf("Value checked at index [%lu] = [%d]\n", list->index,
+			       list->n);
+			if (list->n == value)
+				return (list);
+			list = list->next;
+		}
 	}
-	if (skipnext != NULL)
-	{
-		//defautl print
-		printf("Value checked at index [%lu] = [%d]\n", skipprev->index, skipprev->index);
-	}
-	else
-	{
-		mainptr = skipprev;
-		while (mainptr->next != NULL)
-			mainptr = mainptr->next;
-		//defautl print
-		printf("Value checked at index [%lu] = [%d]\n", skipprev->index, mainptr->index);
-	}
-	mainptr = skipprev;
-	while (mainptr != skipnext)
-	{
-		//defautl print
-		printf("Value checked at index [%lu] = [%d]\n", mainptr->index, mainptr->n);
-		if (mainptr->n == value)
-			return (mainptr);
-		mainptr = mainptr->next;
-	}
-	if (mainptr != NULL && mainptr->n == value)
-	{
-		//defautl print
-		printf("Value checked at index [%lu] = [%d]\n", mainptr->index, mainptr->n);
-		return (mainptr);
-	}
-	//default return
 	return (NULL);
 }
