1ba32c89fbd840697624a5454815eab336e279aa
Fesus Rocuts <fesusrocuts@gmail.com>
2020-11-18 21:24:22 -0500

    t0 v0

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,55 @@
+#!/usr/bin/node
+/**
+ * 0x0F. Star Wars API
+ */
+if (process.argv.length !== 3) {
+  process.exit(0)
+}
+
+const request = require('request')
+const command1 = 'films/' + process.argv[2]
+const urlApi = 'https://swapi-api.hbtn.io/api/'
+const request1 = urlApi + command1
+let list = []
+let currentId = -1
+let limitMax = 0
+
+/**
+ * loadUrlApi fn that connect with api
+ *
+ * @return void and load default fn to read names
+ */
+function loadUrlApi () {
+  request(request1, function (error, response, body) {
+    if (error) {
+      console.log(error)
+    }
+    list = JSON.parse(body).characters
+    currentId = 0
+    if (Array.isArray(list)) {
+      limitMax = list.length
+      loadFilmName()
+    }
+  })
+}
+
+/**
+ * loadFilmName fn recursive that load film name
+ *
+ * @return void
+ */
+function loadFilmName () {
+  if (limitMax > currentId) {
+    request(list[currentId], function (error, response, body) {
+      if (error) {
+        console.log(error)
+      }
+      const name = JSON.parse(body).name
+      console.log(name)
+      loadFilmName()
+    })
+    currentId++
+  }
+}
+
+loadUrlApi()
