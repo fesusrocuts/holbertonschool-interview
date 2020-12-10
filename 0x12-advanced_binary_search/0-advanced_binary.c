c223f017993f9f8d5ae24836098f525dfb849337
Fesus Rocuts <fesusrocuts@gmail.com>
2020-12-09 23:46:21 -0500

    t0 v5

---
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "search_algos.h"
+
+int advanced_binary(int *array, size_t size, int value)
+{
+	size_t i = 0, _continue = 0;
+
+	if (array == NULL){
+		return (-1);
+	}
+	for (i = 0; i < size; i++){
+		if (array[i] == value){
+			_continue = 1;
+		}
+	}
+	if (_continue == 0){
+		return (-1);
+	}
+	return (recursive1(array, array, size, value));
+}
+
+int recursive1(int *array, int *clone, size_t size, int value)
+{
+	size_t idx_num_split = (size - 1) / 2;
+	size_t i = 0;
+	// char extstr[] = "  ";
+
+	if (size < 1)
+		return (-1);
+	printf("Searching in array: ");
+	for (i = 0; i < size; i++){
+		if (i < size - 1){
+			printf("%d%s", clone[i], ", ");
+		}
+		else
+		{
+			printf("%d%s", clone[i], "\n");
+		}
+	}
+	if (!idx_num_split && *clone >= value){
+		if (*clone == value){
+			return (clone - array);
+		}else{
+			return (-1);
+		}
+	}
+	if (clone[idx_num_split] >= value){
+		return (recursive1(array, clone, 1 + idx_num_split, value));
+	}
+	return (recursive1(array, clone + 1 + idx_num_split, size - 1 - idx_num_split, value));
+}
---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,7 @@
+#ifndef SORTING_ALGOS_H
+#define SORTING_ALGOS_H
+
+int advanced_binary(int *array, size_t size, int value);
+int recursive1(int *array, int *clone, size_t size, int value);
+
+#endif
